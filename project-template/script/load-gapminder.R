9fd96da26535301c0ba9c0f137254bbf62f11efc
Tom Russell <tomalrussell@gmail.com>
2017-03-13 22:47:13 +0000

    move 'munge' to 'script'

---
+++ project-template/script/load-gapminder.R
@@ -0,0 +1,2 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "data/gapminder-FiveYearData.csv")
+
